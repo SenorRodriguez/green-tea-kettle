0b9522058527392fa7bfaf4df32a73104a08c9b2
SenorRodriguez <zbig83@gmail.com>
2016-02-26 08:21:55 +0100

    Search ROM
    1 Wire search rom implemented, dirty code

--- main.c
+++ main.c
@@ -2,13 +2,44 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "uart.h"
+#include "onewire.h"
 #include "ds18b20.h"
 #include <stdio.h>
 
 #define _DEBUG_ 0
 
 int main (void) {
-	uart_tx_init (); //UART initialization
+	int64_t id = 0;
+	uint8_t LD =0 ;			//lastDeviation,
+	uart_tx_init ();	//UART initialization
+	//_delay_ms(250);
+	uart_print("\e[1;1H\e[2J");
+	char buffer[50];
+	do 
+	{
+		LD = ow_enumerate(&id, LD);
+		if (LD==0xff) 
+		{
+			uart_print("\n\rFailed\n\r");
+			break;
+		}
+		else
+		{
+			uart_print("\n\r");
+			for(int i=0; i<64; i++)
+			{
+				if (id & ((int64_t)1<<(63-i)))
+					uart_print("1");
+				else
+					uart_print("0");
+			}
+		}
+		snprintf(buffer, 50, "\n\rLD = %d\n\r", LD);
+		uart_print(buffer);
+	}
+	while (LD);
+	 
+	uart_print("\n\r");
 	while (1) {
 	#if _DEBUG_ == 1
 		uint8_t ow_status = ds18b20_reset();
@@ -23,10 +54,17 @@ int main (void) {
 		//double temp = (double)temp_raw/16;	//because 4 less significant bits mean fractions of degree
 		char s_whole_degrees[10];
 		snprintf(s_whole_degrees, 10, "%d", (int)temp_raw>>4);
-		uart_print("\e[1;1H\e[2J");				//clear terminal window
+		//uart_print("\e[1;1H\e[2J");				//clear terminal window
 		uart_print("Current temperature: ");
 		uart_print(s_whole_degrees);
-		uart_print("\n\r");
+		uart_print("\r");
+		/*
+		uint8_t low = temp_raw & 0xff;
+		uint8_t high = (uint8_t)(temp_raw >> 8);
+		uart_send_byte(high);
+		uart_send_byte(low);
+		uart_send_byte(0xff);
+		*/
 		_delay_ms(500);
 	}
 	return 0;
--- onewire.c
+++ onewire.c
@@ -12,18 +12,18 @@
 uint8_t ow_reset() {
 	uint8_t i;
 
-	//to reset, hold the line low for 480 to 640us
+	//to reset, hold the line low 
 	OW_PORT &= ~ (1<<OW_DQ);	//set register to low
 	OW_DDR |= (1<<OW_DQ);		//make it happen, by setting DQ as output
-	_delay_us(500);				//500us, just in case		
+	_delay_us(DELAY_H);				
 
 	//release the line and wait for 15 to 60us
 	OW_DDR &= ~(1<<OW_DQ);		//set DQ as high impedance input, thus release line
-	_delay_us(60);				//wait
+	_delay_us(DELAY_I);				//wait
 
 	//get DQ state and wait 420us
 	i = (OW_PIN & (1<<OW_DQ));
-	_delay_us(420);
+	_delay_us(DELAY_J);
 
 	// 0=at least one device present, 1=no devices detected
 	return i;
@@ -33,7 +33,7 @@ void ow_writebit(uint8_t bit){
 	//put line low for a while, by doing so we say to slaves we want to communicate to them.
 	OW_PORT &= ~ (1<<OW_DQ);	//set a bit in register to O
 	OW_DDR |= (1<<OW_DQ);		//set DQ as output
-	_delay_us(1);
+	_delay_us(DELAY_A);			//delay A
 
 	//if we want to write 1, release the line (it goes back to HI by pull-up resistor)
 	if(bit)
@@ -47,27 +47,27 @@ void ow_writebit(uint8_t bit){
 uint8_t ow_readbit(void){
 	uint8_t bit=0;
 
-	//pull line low for 1us
+	//pull line low for A us
 	OW_PORT &= ~ (1<<OW_DQ);		//same procedure as in ow_writebit
 	OW_DDR |= (1<<OW_DQ);
-	_delay_us(1);
+	_delay_us(DELAY_A);
 
-	//release line and wait for 15us max
+	//release line and wait for E us
 	OW_DDR &= ~(1<<OW_DQ);			//set as input
-	_delay_us(14);					//14us, just in case
+	_delay_us(DELAY_E);					//14us, just in case
 
 	//read the value
 	if(OW_PIN & (1<<OW_DQ))
 	bit = 1;
 
 	//wait another 45us to fill the 60us read window
-	_delay_us(45);
+	_delay_us(DELAY_F);
 	return bit;
 }
 void ow_writebyte(uint8_t data){
 	uint8_t i=8;
 	while(i--){					//repeat 8 times
-		ow_writebit(data & 1);	//take LSB ant transmit
+		ow_writebit(data & 1);	//take LSB and transmit
 		data >>= 1;				//shift right
 	}
 }
@@ -79,4 +79,52 @@ uint8_t ow_readbyte(void){
 		byte |= (ow_readbit() << 7);
 	}
 	return byte;
+}
+
+
+//enumerate up to MAX_DEVICES devices on 1-Wire bus 
+uint8_t ow_enumerate(volatile uint64_t* bitPattern, uint8_t lastDeviation){
+	//init variables
+	uint8_t newDeviation = 0;
+	uint8_t bitIndex = 0;
+    uint8_t bit1,bit2;
+	ow_reset();
+	ow_writebyte(OW_CMD_SEARCHROM);
+	while (bitIndex<64)
+	{
+		bit1 = ow_readbit();
+		bit2 = ow_readbit();
+		if (bit1 & bit2) return ERR_ROM_SEARCH_FAILED;	//if both bits are 1, return error
+		
+		if (bit1 | bit2){
+			//if one of two bits is 1 it means we have at least 2 devices, and their addresses are the same up to index [bitIndex]
+			if (bit1)
+			*bitPattern |= (uint64_t)1<<bitIndex;		//store 1
+			else
+			*bitPattern &= ~((uint64_t)1<<bitIndex);	//store 0
+		}
+		else{
+			//if both bits are 0 it means we have at least 2 devices, and their addresses differ at index [bitIndex]
+			if (bitIndex==lastDeviation){
+				*bitPattern |= (uint64_t)1 << bitIndex;					//set bitPattert[bitIndex] to 1
+			}
+			else{ 
+				if(bitIndex>lastDeviation) {
+					*bitPattern &= ~((uint64_t)1 << bitIndex);			//set bitPattert[bitIndex] to 0
+					newDeviation = bitIndex;
+				}
+				else{
+					if (!(*bitPattern & ((uint64_t)1 << bitIndex))){	//if bitPattern[bitIndex]==0
+						newDeviation = bitIndex; 
+					}
+				}
+			}
+		}
+		if (*bitPattern & (uint64_t)1 << bitIndex)						//test bitPattern[bitIndex]
+			ow_writebit(1); 
+		else	
+			ow_writebit(0);
+		bitIndex++;
+	}
+	return newDeviation;
 }
